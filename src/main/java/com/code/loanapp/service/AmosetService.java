d7e3584b0effbc611d3bad3f0924112fad88464a
kabugu arafat <59297807+code-oss@users.noreply.github.com>
2023-09-17 15:10:10 +0300

    Update AmosetService.java

--- src/main/java/com/code/loanapp/service/AmosetService.java
+++ src/main/java/com/code/loanapp/service/AmosetService.java
@@ -9,8 +9,8 @@ import com.code.loanapp.model.Amoset;
 public interface AmosetService {
 	List<Amoset> getAllAmoset();
 	void saveAmoset(Amoset amoset);
-	Amoset getAmosetById(long code);
-	void deleteAmosetById(long code);
+	Amoset getAmosetById(String code);
+	void deleteAmosetById(String code);
 	Page<Amoset> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 	public BigDecimal sumLoan();
 	public BigDecimal sumtotal();
