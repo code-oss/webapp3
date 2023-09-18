0d2ed26860f9c7b5b82e0ebe8c66b1bb87eba2f5
kabugu arafat <59297807+code-oss@users.noreply.github.com>
2023-09-18 20:39:20 +0300

    Update ClientController.java

--- src/main/java/com/code/loanapp/controller/ClientController.java
+++ src/main/java/com/code/loanapp/controller/ClientController.java
@@ -1,26 +1,29 @@
-package com.code.loanapp.controller;
+package com.code.amortization.controller;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.util.List;
 
+import com.code.amortization.model.AdminModel;
+import com.code.amortization.model.Amoset;
+import com.code.amortization.model.Clients;
+import com.code.amortization.repository.AdminModelRep;
+import com.code.amortization.service.AdminService;
+import com.code.amortization.service.AmosetAddService;
+import com.code.amortization.service.AmosetService;
+import com.code.amortization.service.ClientService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.code.loanapp.model.Amoset;
-import com.code.loanapp.model.Clients;
-import com.code.loanapp.service.AmosetAddService;
-import com.code.loanapp.service.AmosetService;
-import com.code.loanapp.service.ClientService;
+
 
 @Controller
 public class ClientController {
@@ -33,128 +36,216 @@ public class ClientController {
 
 	@Autowired
 	private AmosetAddService amosetAddService;
-	
+	@Autowired
+	private AdminService adminService;
+
+	@Autowired
+	private AdminModelRep adminModelRep;
+
 	@GetMapping("/home/viewClients")
 	public String viewHomePage(Model model) {
-		return findPaginated(1, "id", "asc", model);		
+		return findPaginated(1, "id", "asc", model);
 	}
-	
+	@GetMapping("/access_admin")
+	public String LoginAdmin(){
+		return "AdminDataLogin";
+	}
+	//view admins
+	@GetMapping("/home/viewAdmins")
+	public String viewAdmins(Model model) {
+		return findPaginated4(1, "id", "asc", model);
+	}
 	@GetMapping("/home/addClient")
 	public String showNewClientForm(Model model) {
 		Clients client = new Clients();
 		model.addAttribute("client", client);
 		return "AddClient";
 	}
-	
+	//admin add page
+	@GetMapping("/home/addAdmin")
+	public String showNewAdminForm(Model model) {
+		AdminModel adminModel = new AdminModel();
+		model.addAttribute("admins", adminModel);
+		return "AddAdmin";
+	}
+
 	@PostMapping("/saveClient")
 	public String saveClient(@ModelAttribute("client") Clients client) {
 		clientService.saveClient(client);
 		return "redirect:/home/viewClients";
 	}
-	
+	//admin add page action
+	@PostMapping("/saveAdmin")
+	public String saveAdmin(@ModelAttribute("admins") AdminModel adminModel) {
+		adminService.saveAdmin(adminModel);
+		return "redirect:/home/viewAdmins";
+	}
+
+
 	@GetMapping("/showFormForUpdate/{id}")
-	public String showFormForUpdate(@PathVariable ( value = "id") String id, Model model) {
-		
+	public String showFormForUpdate(@PathVariable ( value = "id") long id, Model model) {
+
 
 		Clients client = clientService.getClientById(id);
 
 		model.addAttribute("client", client);
 		return "UpdateClient";
 	}
+	//admin update page
+	@GetMapping("/showFormForUpdateAdmin/{id}")
+	public String showFormForUpdateAdmin(@PathVariable ( value = "id") long id, Model model) {
+
+
+		AdminModel adminModel = adminService.getAdminById(id);
+
+		model.addAttribute("admin", adminModel);
+		return "UpdateAdmin";
+	}
 
 	@GetMapping("/clientView/{id}")
-	public String showClient(@PathVariable ( value = "id") String id, Model model) {
-		
+	public String showClient(@PathVariable ( value = "id") long id, Model model) {
+
 
 		Clients client = clientService.getClientById(id);
 
 		model.addAttribute("client", client);
 		return "ViewClient";
 	}
-	
+
 	@GetMapping("/deleteClient/{id}")
-	public String deleteClient(@PathVariable (value = "id") String id) {
-		
+	public String deleteClient(@PathVariable (value = "id") long id) {
+
 		this.clientService.deleteClientById(id);
 		return "redirect:/home/viewClients";
 	}
-	
-	
+	//delete admin
+	@GetMapping("/deleteAdmin/{id}")
+	public String deleteAdmin(@PathVariable (value = "id") long id) {
+
+		this.adminService.deleteAdminById(id);
+		return "redirect:/home/viewAdmins";
+	}
+
+	//access admin settings
+	@GetMapping("/Signin2")
+	public String Login2(@RequestParam("username")String username, @RequestParam("password")String password , HttpSession
+			session, ModelMap modelMap){
+		AdminModel adminModel = adminModelRep.findAdminByUsernamePassword(username,password);
+
+		if (adminModel !=null){
+			String adminUser = adminModel.getUsername();
+			String adminPass = adminModel.getPassword();
+
+			if (username.equalsIgnoreCase(adminUser) && password.equalsIgnoreCase(adminPass)){
+				session.setAttribute(username, "username");
+				return "redirect:/home/viewAdmins";
+			} else {
+				modelMap.put("error", "details Un recorgnised");
+				return "AdminDataLoginErr";
+			}
+		} else {
+			modelMap.put("error", "details Un recorgnized");
+			return "AdminDataLoginErr";
+		}
+	}
+
+
 	@GetMapping("/page/{pageNo}")
-	public String findPaginated(@PathVariable (value = "pageNo") int pageNo, 
+	public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
 			@RequestParam("sortField") String sortField,
 			@RequestParam("sortDir") String sortDir,
 			Model model) {
 		int pageSize = 10;
-		
+
 		Page<Clients> page = clientService.findPaginated(pageNo, pageSize, sortField, sortDir);
 		List<Clients> listClients = page.getContent();
-		
+
 		model.addAttribute("currentPage", pageNo);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("totalItems", page.getTotalElements());
-		
+
 		model.addAttribute("sortField", sortField);
 		model.addAttribute("sortDir", sortDir);
 		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-		
+
 		model.addAttribute("listClients", listClients);
 		return "ClientData";
 	}
-	
+
 
 	@GetMapping("/pages/{pageNos}")
-	public String findPaginated1(@PathVariable (value = "pageNo") int pageNo, 
+	public String findPaginated1(@PathVariable (value = "pageNo") int pageNo,
 			@RequestParam("sortField") String sortField,
 			@RequestParam("sortDir") String sortDir,
 			Model model) {
 		int pageSize = 10;
-		
+
 		Page<Clients> page = clientService.findPaginated(pageNo, pageSize, sortField, sortDir);
 		List<Clients> listClients = page.getContent();
-		
+
 		model.addAttribute("currentPage", pageNo);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("totalItems", page.getTotalElements());
-		
+
 		model.addAttribute("sortField", sortField);
 		model.addAttribute("sortDir", sortDir);
 		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-		
+
 		model.addAttribute("listClients", listClients);
-		return "/AmortizationSettings";
+		return "AmortizationSettings";
 	}
 	@GetMapping("/pages1/{pageNos1}")
-	public String findPaginated2(@PathVariable (value = "pageNos1") int pageNo, 
+	public String findPaginated2(@PathVariable (value = "pageNos1") int pageNo,
 			@RequestParam("sortField") String sortField,
 			@RequestParam("sortDir") String sortDir,
 			Model model) {
 		int pageSize = 10;
-		
+
 		Page<Amoset> page = amosetService.findPaginated(pageNo, pageSize, sortField, sortDir);
 		List<Amoset> listAmosets = page.getContent();
-		
+
 		model.addAttribute("currentPage", pageNo);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("totalItems", page.getTotalElements());
-		
+
 		model.addAttribute("sortField", sortField);
 		model.addAttribute("sortDir", sortDir);
 		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-		
+
 		model.addAttribute("listAmosets", listAmosets);
-		return "/AmortizationData";
+		return "AmortizationData";
+	}
+	@GetMapping("/pages4/{pageNos4}")
+	public String findPaginated4(@PathVariable (value = "pageNos4") int pageNo,
+								 @RequestParam("sortField") String sortField,
+								 @RequestParam("sortDir") String sortDir,
+								 Model model) {
+		int pageSize = 10;
+
+		Page<AdminModel> page = adminService.findPaginated(pageNo, pageSize, sortField, sortDir);
+		List<AdminModel> listAdmins = page.getContent();
+
+		model.addAttribute("currentPage4", pageNo);
+		model.addAttribute("totalPages4", page.getTotalPages());
+		model.addAttribute("totalItems4", page.getTotalElements());
+
+		model.addAttribute("sortField4", sortField);
+		model.addAttribute("sortDir4", sortDir);
+		model.addAttribute("reverseSortDir4", sortDir.equals("asc") ? "desc" : "asc");
+
+		model.addAttribute("admins", listAdmins);
+		return "AdminData";
 	}
 
 	//amortization settings
 
 	@GetMapping("/home/amosetting")
 	public String viewAmoset(Model model) {
-		return findPaginated1(1, "id", "asc", model);		
+		return findPaginated1(1, "id", "asc", model);
 	}
 	@GetMapping("/home/amo_settings_data")
 	public String viewAmoset_data(Model model) {
-		return findPaginated2(1, "code", "asc", model);		
+		return findPaginated2(1, "code", "asc", model);
 	}
 
 	@PostMapping("/saveAmoset")
@@ -172,8 +263,8 @@ public class ClientController {
     }
 
 	@GetMapping("/showFormForLoanUpdate/{id}")
-	public String showFormForLoanUpdate(@PathVariable ( value = "id") String id, Model model) {
-		
+	public String showFormForLoanUpdate(@PathVariable ( value = "id") long id, Model model) {
+
 
 		Clients client = clientService.getClientById(id);
 
@@ -183,8 +274,8 @@ public class ClientController {
 
 
 	@GetMapping("/AmosetView/{code}")
-	public String showAmoset(@PathVariable ( value = "code") String code, Model model) {
-		
+	public String showAmoset(@PathVariable ( value = "code") long code, Model model) {
+
 
 		Amoset amoset = amosetService.getAmosetById(code);
 
@@ -192,11 +283,11 @@ public class ClientController {
 		return "ViewAmoset";
 	}
 	@GetMapping("/deleteAmoset/{code}")
-	public String deleteAmoset(@PathVariable (value = "code") String code) {
-		
+	public String deleteAmoset(@PathVariable (value = "code") long code) {
+
 		this.amosetService.deleteAmosetById(code);
 		return "redirect:/";
 	}
 
-	
+
 }
