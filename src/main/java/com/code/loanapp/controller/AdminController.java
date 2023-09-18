aa0169fa5c48530d2596c843eaabed5f4d2b3379
kabugu arafat <59297807+code-oss@users.noreply.github.com>
2023-09-18 20:38:25 +0300

    Update AdminController.java

--- src/main/java/com/code/loanapp/controller/AdminController.java
+++ src/main/java/com/code/loanapp/controller/AdminController.java
@@ -1,32 +1,30 @@
-package com.code.loanapp.controller;
+package com.code.amortization.controller;
 
-import java.math.BigDecimal;
-import java.util.List;
 
+
+import com.code.amortization.dataExports.domain_clients.ClientExportService;
+import com.code.amortization.dataExports.domain_transactions.TransactionExportService;
+import com.code.amortization.model.AdminModel;
+import com.code.amortization.repository.AdminModelRep;
+import com.code.amortization.service.AdminModelserv;
+import com.code.amortization.service.AmosetService;
+import com.code.amortization.service.ClientService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.integration.IntegrationProperties.RSocket.Client;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.code.loanapp.model.AdminModel;
-import com.code.loanapp.model.Amoset;
-import com.code.loanapp.model.Clients;
-import com.code.loanapp.repository.AdminModelRep;
-import com.code.loanapp.service.AdminModelserv;
-import com.code.loanapp.service.AmosetService;
-import com.code.loanapp.service.ClientService;
 
-import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
 @Controller
 public class AdminController {
-     @Autowired
+    @Autowired
     private AdminModelRep adminModelRep;
 
     @Autowired
@@ -38,6 +36,12 @@ public class AdminController {
     @Autowired
     private ClientService clientService;
 
+    @Autowired
+    private ClientExportService clientExportService;
+
+    @Autowired
+    private TransactionExportService transactionExportService;
+
     @GetMapping("/")
     public String LoginHome(){
         return "Enter";
@@ -47,12 +51,12 @@ public class AdminController {
     public String viewIndex(Model model) {
         List<AdminModel>adminModels= adminModelserv.getAdminModels();
         model.addAttribute("adminModels" ,adminModels);
-		return "index";
-	}
+        return "index";
+    }
 
     @GetMapping("/Signin")
     public String Login(@RequestParam("username")String username, @RequestParam("password")String password , HttpSession
-                        session, ModelMap modelMap){
+            session, ModelMap modelMap){
         AdminModel adminModel = adminModelRep.findAdminByUsernamePassword(username,password);
 
         if (adminModel !=null){
@@ -64,19 +68,40 @@ public class AdminController {
                 return "redirect:/home/dashboard";
             } else {
                 modelMap.put("error", "details Un recorgnised");
-                return "/ErrorPa";
+                return "ErrorPa";
             }
         } else {
             modelMap.put("error", "details Un recorgnized");
-            return "/ErrorPa";
+            return "ErrorPa";
         }
     }
 
+
     @GetMapping(value = "/home/logout")
-	public String logout_user(HttpSession session)
-	{
-		session.removeAttribute("username");
-		session.invalidate();
-		return "redirect:/";
-	}
+    public String logout_user(HttpSession session)
+    {
+        session.removeAttribute("username");
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    @GetMapping("/export-to-excel-clients")
+    public String exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=Clients_Information.xlsx";
+        response.setHeader(headerKey, headerValue);
+        clientExportService.exportCToExcel(response);
+        return "/home/dashboard";
+    }
+
+    @GetMapping("/export-to-excel-transactions")
+    public String exportToExcel2(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=Transactions_Information.xlsx";
+        response.setHeader(headerKey, headerValue);
+        transactionExportService.exportCToExcel2(response);
+        return "/home/dashboard";
+    }
 }
